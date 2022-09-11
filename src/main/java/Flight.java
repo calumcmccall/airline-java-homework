272feaa72609301c453bf0c76d1003f87ec2dfbc
Calum <calumcmccall@gmail.com>
2022-09-11 19:26:09 +0100

    added and tested flight

---
+++ src/main/java/Flight.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+
+public class Flight {
+
+    private ArrayList<Pilot> pilots;
+    private ArrayList<CabinCrewMember> cabinCrew;
+    private ArrayList<Passenger> passengers;
+    private Plane plane;
+    private String flightNumber;
+    private String destination;
+    private String departure;
+    private String departureTime;
+
+    public Flight(ArrayList<Pilot> pilots, ArrayList<CabinCrewMember> cabinCrew, ArrayList<Passenger> passengers, Plane plane, String flightNumber, String destination, String departure, String departureTime) {
+        this.pilots = pilots;
+        this.cabinCrew = cabinCrew;
+        this.passengers = passengers;
+        this.plane = plane;
+        this.flightNumber = flightNumber;
+        this.destination = destination;
+        this.departure = departure;
+        this.departureTime = departureTime;
+    }
+
+    public ArrayList<Pilot> getPilots() {
+        return pilots;
+    }
+
+    public ArrayList<CabinCrewMember> getCabinCrew() {
+        return cabinCrew;
+    }
+
+    public ArrayList<Passenger> getPassengers() {
+        return passengers;
+    }
+
+    public Plane getPlane() {
+        return plane;
+    }
+
+    public String getFlightNumber() {
+        return flightNumber;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public String getDeparture() {
+        return departure;
+    }
+
+    public String getDepartureTime() {
+        return departureTime;
+    }
+
+}
---
+++ src/test/java/FlightTest.java
@@ -0,0 +1,76 @@
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.assertEquals;
+
+public class FlightTest {
+
+    Flight flight;
+
+    @Before
+    public void setUp(){
+        Pilot pilot = new Pilot("Tom", Rank.CAPTAIN, "A426");
+        ArrayList<Pilot> pilots = new ArrayList<Pilot>();
+        pilots.add(pilot);
+
+        CabinCrewMember cabinCrewMember1 = new CabinCrewMember("Ryan", Rank.FLIGHTATTENDANT);
+        CabinCrewMember cabinCrewMember2 = new CabinCrewMember("Megan", Rank.FLIGHTATTENDANT);
+        ArrayList<CabinCrewMember> cabinCrew = new ArrayList<CabinCrewMember>();
+        cabinCrew.add(cabinCrewMember1);
+        cabinCrew.add(cabinCrewMember2);
+
+        Passenger passenger1 = new Passenger("Susan", 3);
+        Passenger passenger2 = new Passenger("Angela", 2);
+        Passenger passenger3 = new Passenger("Max", 4);
+        ArrayList<Passenger> passengers = new ArrayList<Passenger>();
+        passengers.add(passenger1);
+        passengers.add(passenger2);
+        passengers.add(passenger3);
+
+        Plane plane = new Plane(PlaneType.BOEING747);
+
+        flight = new Flight(pilots, cabinCrew, passengers, plane, "FR756", "GLA", "EDI", "09:42");
+    }
+
+    @Test
+    public void testGetPilots() {
+        assertEquals(1, flight.getPilots().size());
+    }
+
+    @Test
+    public void testGetCabinCrew() {
+        assertEquals(2, flight.getCabinCrew().size());
+    }
+
+    @Test
+    public void testGetPassengers() {
+        assertEquals(3, flight.getPassengers().size());
+    }
+
+    @Test
+    public void testGetPlane() {
+        assertEquals(PlaneType.BOEING747, flight.getPlane().getPlaneType());
+    }
+
+    @Test
+    public void testGetFlightNumber() {
+        assertEquals("FR756", flight.getFlightNumber());
+    }
+
+    @Test
+    public void testGetDestination() {
+        assertEquals("GLA", flight.getDestination());
+    }
+
+    @Test
+    public void testGetDeparture() {
+        assertEquals("EDI", flight.getDeparture());
+    }
+
+    @Test
+    public void testGetDepartureTime() {
+        assertEquals("09:42", flight.getDepartureTime());
+    }
+}
